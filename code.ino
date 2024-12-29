546b34be635bca517e34203e8395d04efb129d8a
Flavio Luiz Lopes Arruda <67842829+FlazO0@users.noreply.github.com>
2024-12-28 23:01:54 -0300

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,114 +1,141 @@
-#define CUSTOM_SETTINGS
-#define INCLUDE_GAMEPAD_MODULE
-#include <DabbleESP32.h>
+#include "BluetoothSerial.h"
+#include <Arduino.h>
 
-//Right motor
-int enableRightMotor=22; 
-int rightMotorPin1=16;
-int rightMotorPin2=17;
-//Left motor
-int enableLeftMotor=23;
-int leftMotorPin1=18;
-int leftMotorPin2=19;
+BluetoothSerial serialBT;
 
-#define MAX_MOTOR_SPEED 255
+// Bluetooth signal store in this variable
+char btSignal;
 
-const int PWMFreq = 1000; /* 1 KHz */
-const int PWMResolution = 8;
-const int rightMotorPWMSpeedChannel = 4;
-const int leftMotorPWMSpeedChannel = 5;
+// Initial speed
+int Speed = 100;
 
-void rotateMotor(int rightMotorSpeed, int leftMotorSpeed)
-{
-  if (rightMotorSpeed < 0)
-  {
-    digitalWrite(rightMotorPin1,LOW);
-    digitalWrite(rightMotorPin2,HIGH);    
-  }
-  else if (rightMotorSpeed > 0)
-  {
-    digitalWrite(rightMotorPin1,HIGH);
-    digitalWrite(rightMotorPin2,LOW);      
-  }
-  else
-  {
-    digitalWrite(rightMotorPin1,LOW);
-    digitalWrite(rightMotorPin2,LOW);      
-  }
+// Define PWM pins
+int enA = 5;
+int enB = 23;
 
-  if (leftMotorSpeed < 0)
-  {
-    digitalWrite(leftMotorPin1,LOW);
-    digitalWrite(leftMotorPin2,HIGH);    
-  }
-  else if (leftMotorSpeed > 0)
-  {
-    digitalWrite(leftMotorPin1,HIGH);
-    digitalWrite(leftMotorPin2,LOW);      
-  }
-  else
-  {
-    digitalWrite(leftMotorPin1,LOW);
-    digitalWrite(leftMotorPin2,LOW);      
-  }
+// Motor controlling pins
+int IN1 = 22;
+int IN2 = 21;
+int IN3 = 19;
+int IN4 = 18;
 
-  ledcWrite(rightMotorPWMSpeedChannel, abs(rightMotorSpeed));
-  ledcWrite(leftMotorPWMSpeedChannel, abs(leftMotorSpeed));  
+// PWM frequency and resolution
+const int pwmFreq = 5000; // 5 kHz
+const int pwmResolution = 8; // 8-bit
+
+void setup() {
+  Serial.begin(115200);
+
+  // Bluetooth initialization
+  serialBT.begin("CARRINHO ;)");
+
+  // Set pin modes for motor control
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  // Attach PWM to pins with specified frequency and resolution
+  ledcAttach(enA, pwmFreq, pwmResolution); // Attach enA pin with PWM frequency and resolution
+  ledcAttach(enB, pwmFreq, pwmResolution); // Attach enB pin with PWM frequency and resolution
+
+  // Initial state of the car
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
 }
 
-void setUpPinModes()
-{
-  pinMode(enableRightMotor,OUTPUT);
-  pinMode(rightMotorPin1,OUTPUT);
-  pinMode(rightMotorPin2,OUTPUT);
+void loop() {
+  while (serialBT.available()) {
+    btSignal = serialBT.read();
+    
+    // Set speed based on Bluetooth signal
+    if (btSignal == '0') Speed = 100;
+    if (btSignal == '1') Speed = 110;
+    if (btSignal == '2') Speed = 120;
+    if (btSignal == '3') Speed = 130;
+    if (btSignal == '4') Speed = 140;
+    if (btSignal == '5') Speed = 150;
+    if (btSignal == '6') Speed = 180;
+    if (btSignal == '7') Speed = 200;
+    if (btSignal == '8') Speed = 220;
+    if (btSignal == '9') Speed = 240;
+    if (btSignal == 'q') Speed = 255;
 
-  pinMode(enableLeftMotor,OUTPUT);
-  pinMode(leftMotorPin1,OUTPUT);
-  pinMode(leftMotorPin2,OUTPUT);
-
-  //Set up PWM for speed
-  ledcSetup(rightMotorPWMSpeedChannel, PWMFreq, PWMResolution);
-  ledcSetup(leftMotorPWMSpeedChannel, PWMFreq, PWMResolution);  
-  ledcAttachPin(enableRightMotor, rightMotorPWMSpeedChannel);
-  ledcAttachPin(enableLeftMotor, leftMotorPWMSpeedChannel); 
-
-  rotateMotor(0,0); 
+    // Output signal to Serial Monitor
+    Serial.println(btSignal);
+    
+    // Control motor direction based on Bluetooth signal
+    if (btSignal == 'B') {
+      backward();
+    } else if (btSignal == 'F') {
+      forward();
+    } else if (btSignal == 'L') {
+      turnLeft();  // Adjusted turning left behavior
+    } else if (btSignal == 'R') {
+      turnRight(); // Adjusted turning right behavior
+    } else if (btSignal == 'S') {
+      stop();
+    }
+  }
 }
 
-void setup()
-{
-  setUpPinModes();
-  Dabble.begin("MyBluetoothCar"); 
+// Function to move motor backward
+void backward() {
+  ledcWrite(enA, Speed);
+  ledcWrite(enB, Speed);
+
+  digitalWrite(IN1, LOW);  // Motor A (left) spins backward
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);  // Motor B (right) spins backward
+  digitalWrite(IN4, HIGH);
 }
 
-void loop()
-{
-  int rightMotorSpeed=0;
-  int leftMotorSpeed=0;
-  Dabble.processInput();
-  if (GamePad.isUpPressed())
-  {
-    rightMotorSpeed = MAX_MOTOR_SPEED;
-    leftMotorSpeed = MAX_MOTOR_SPEED;
-  }
+// Function to move motor forward
+void forward() {
+  ledcWrite(enA, Speed);
+  ledcWrite(enB, Speed);
 
-  if (GamePad.isDownPressed())
-  {
-    rightMotorSpeed = -MAX_MOTOR_SPEED;
-    leftMotorSpeed = -MAX_MOTOR_SPEED;
-  }
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
 
-  if (GamePad.isLeftPressed())
-  {
-    rightMotorSpeed = MAX_MOTOR_SPEED;
-    leftMotorSpeed = -MAX_MOTOR_SPEED;
-  }
+// Function to turn left (disable right motor, only left motor moves)
+void turnLeft() {
+  // Stop right motor, keep left motor running
+  ledcWrite(enA, Speed);  // Keep left motor running
+  ledcWrite(enB, 0);      // Stop right motor
 
-  if (GamePad.isRightPressed())
-  {
-    rightMotorSpeed = -MAX_MOTOR_SPEED;
-    leftMotorSpeed = MAX_MOTOR_SPEED;
-  }
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);  // Stop right motor
+}
 
-  rotateMotor(rightMotorSpeed, leftMotorSpeed);
+// Function to turn right (disable left motor, only right motor moves)
+void turnRight() {
+  // Stop left motor, keep right motor running
+  ledcWrite(enA, 0);      // Stop left motor
+  ledcWrite(enB, Speed);  // Keep right motor running
+
+  digitalWrite(IN1, LOW);  // Stop left motor
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);  // Right motor moves forward
+}
+
+// Function to stop both motors
+void stop() {
+  ledcWrite(enA, 0); // Set PWM value to 0 to stop the motor
+  ledcWrite(enB, 0); // Set PWM value to 0 to stop the motor
+
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
 }
